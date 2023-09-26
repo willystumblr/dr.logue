dd10c9d1fd7824f2e0a0352998fbc0bd64f4dfa0
Minseo Kim <minskim010203@gmail.com>
2023-09-26 16:46:20 +0900

    Q2 solved

--- preliminary/source/문제2/Q2.py
+++ preliminary/source/문제2/Q2.py
@@ -3,11 +3,16 @@
 '''
 
 import argparse
+#import wave
+import json
+import pydub
+from pydub.exceptions import CouldntDecodeError
+import tqdm 
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='Korean SR Contest 2023')
-    parser.add_argument('--audiolist', type=str)
-    parser.add_argument('--outfile', type=str)
+    parser.add_argument('audiolist', type=str)
+    parser.add_argument('outfile', type=str)
 
     args = parser.parse_args()
 
@@ -21,22 +26,32 @@ def arg_parse():
 def detect_wav_error(file_list, out_file):
     #
     # YOUR CODE HRER
-    pass
-
-
-def detect_silence(file_list, out_file):
-    #
-    # YOUR CODE HRER
-    pass
+    error_files = list()
+    with open(file_list, 'r') as f:
+        for line in tqdm.tqdm(f):
+            file = line.strip()
+        try:
+            audio = pydub.AudioSegment.from_wav(file)
+            if not len(audio):
+                error_files.append(file.replace('./', ''))
+                
+            if audio.max_dBFS >= 0:
+                error_files.append(file.replace('./', ''))
+                
+        except CouldntDecodeError:
+            error_files.append(file.replace('./', ''))
+        
+        except Exception as e:
+            error_files.append(file.replace('./', ''))
+    
+    with open(out_file, 'w') as jsonf:
+        json.dump({"error_list": error_files}, jsonf, indent=4)
 
 
 def main():
     args = arg_parse()
 
     # Q2
-    detect_silence(args.audiolist, args.outfile)
-
-    # Q3
     detect_wav_error(args.audiolist, args.outfile)
 
 
