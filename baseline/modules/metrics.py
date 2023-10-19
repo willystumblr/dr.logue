1f7a539d2f82a5a4ed81de61de223a219e9be92a
Minseo Kim <minskim010203@gmail.com>
2023-10-19 17:05:08 +0900

    import error

--- baseline/modules/metrics.py
+++ baseline/modules/metrics.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from modules.utils import revise
+#from modules.utils import revise
 
 def get_metric(metric_name, vocab):
     if metric_name == 'CER':
